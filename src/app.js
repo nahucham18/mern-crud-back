539d5bc7e397ba8455379cbe4253cdb633fc5ab2
Nahuel Chamorro <118007578+nahucham18@users.noreply.github.com>
2023-08-23 14:17:04 -0300

    env url

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ const swaggerSpec = {
         },
         servers: [
             {
-                url: 'https://mern-crud-back-9v77lpqer-nahucham18.vercel.app' 
+                url: process.env.API_BASE_URL || "http://localhost:3001/"
             },  
         ]
     },
